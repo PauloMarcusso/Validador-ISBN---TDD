4dbaba354d0fd3dc6d2999b00ee5694d75cd9826
Paulo H <phmarcusso1@hotmail.com>
2020-07-08 15:05:51 -0300

    utilizando Mockito

--- src/main/java/com/tdd/isbn/ISBNValidator/GerenciadorEstoque.java
+++ src/main/java/com/tdd/isbn/ISBNValidator/GerenciadorEstoque.java
@@ -2,18 +2,28 @@ package com.tdd.isbn.ISBNValidator;
 
 public class GerenciadorEstoque{
 
-	private ExternalISBNDataService service;
-	
-	public void setService(ExternalISBNDataService service) {
-		this.service = service;
-	}
+	private ExternalISBNDataService webService;
+	private ExternalISBNDataService databaseService;
 
 	public String getLocalizadorCodigo(String isbn) {
-		Livro livro = service.lookup(isbn);
+		Livro livro = databaseService.lookup(isbn);
+		if(livro == null) {
+			livro = webService.lookup(isbn);
+		}
 		StringBuilder localizador = new StringBuilder();
 		localizador.append(isbn.substring(isbn.length()-4)); //pega os ultimos 4 numeros
 		localizador.append(livro.getAutor().substring(0, 1));//pega a primeira letra do nome do autor
 		localizador.append(livro.getTitulo().split(" ").length);//
 		return localizador.toString();
 	}
+
+	public void setWebService(ExternalISBNDataService service) {
+		this.webService = service;
+	}
+
+	public void setDatabaseService(ExternalISBNDataService databaseService) {
+		this.databaseService = databaseService;
+	}
+	
+	
 }
--- src/test/java/com/tdd/isbn/test/ISBNValidator/GerenciadorEstoqueTest.java
+++ src/test/java/com/tdd/isbn/test/ISBNValidator/GerenciadorEstoqueTest.java
@@ -1,7 +1,7 @@
 package com.tdd.isbn.test.ISBNValidator;
 
 import static org.junit.Assert.*;
-
+import static org.mockito.Mockito.*;
 import org.junit.Test;
 
 import com.tdd.isbn.ISBNValidator.ExternalISBNDataService;
@@ -13,20 +13,65 @@ public class GerenciadorEstoqueTest {
 	@Test
 	public void pegaOLocalizadorCodigoCorreto() {
 		
-		ExternalISBNDataService testService = new ExternalISBNDataService() {
+		ExternalISBNDataService testWebService = new ExternalISBNDataService() {
 			
 			public Livro lookup(String isbn) {
 				return new Livro(isbn, "Of Mice And Men", "J. Steinbeck");
 			}
 		};
+		
+		ExternalISBNDataService testDataBaseService = new ExternalISBNDataService() {
+			
+			public Livro lookup(String isbn) {
+				return null;
+			}
+		};
+		
 		GerenciadorEstoque gerenciadorEstoque = new GerenciadorEstoque();
-		gerenciadorEstoque.setService(testService);
+		gerenciadorEstoque.setWebService(testWebService);
+		gerenciadorEstoque.setDatabaseService(testDataBaseService);
 		
 		String isbn = "0140177396";
 		String localizador = gerenciadorEstoque.getLocalizadorCodigo(isbn);
 		assertEquals("7396J4", localizador);
 	}
+	
+	@Test
+	public void databaseEhUsadoSeHouverDados() {
+		ExternalISBNDataService databaseService = mock(ExternalISBNDataService.class);
+		ExternalISBNDataService webService = mock(ExternalISBNDataService.class);
+		
+		when(databaseService.lookup("0140177396")).thenReturn(new Livro("0140177396", "abc", "abc"));
+		
+		GerenciadorEstoque gerenciadorEstoque = new GerenciadorEstoque();
+		gerenciadorEstoque.setWebService(webService);
+		gerenciadorEstoque.setDatabaseService(databaseService);
+		
+		String isbn = "0140177396";
+		String localizador = gerenciadorEstoque.getLocalizadorCodigo(isbn);
+		
+		verify(databaseService, times(1)).lookup("0140177396");
+		verify(webService, times(0)).lookup(anyString());
+	}
 
+	@Test
+	public void webServiceEhUsadoSeNaoHouverDados() {
+		ExternalISBNDataService databaseService = mock(ExternalISBNDataService.class);
+		ExternalISBNDataService webService = mock(ExternalISBNDataService.class);
+		
+		when(databaseService.lookup("0140177396")).thenReturn(null);
+		when(webService.lookup("0140177396")).thenReturn(new Livro("0140177396", "abc", "abc"));
+		
+		GerenciadorEstoque gerenciadorEstoque = new GerenciadorEstoque();
+		gerenciadorEstoque.setWebService(webService);
+		gerenciadorEstoque.setDatabaseService(databaseService);
+		
+		String isbn = "0140177396";
+		String localizador = gerenciadorEstoque.getLocalizadorCodigo(isbn);
+		
+		verify(databaseService, times(1)).lookup("0140177396");
+		verify(webService, times(1)).lookup("0140177396");
+	}
 	
 
 }
